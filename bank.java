7753e71e3534b111aced604e448c8c7970912782
kiranlohare <110059196+kiranlohare@users.noreply.github.com>
2022-07-27 01:12:34 +0530

    Create bank.java

---
+++ bank.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class AccountOpen 
+{
+	String name;
+	String dob;
+	char gender;
+	double mobno;
+	String address;
+	String acctype;
+	String email;
+	String pwd;
+	int ammount;
+	Scanner sc=new Scanner(System.in);
+	public static void printMessage()
+	{
+		
+	}
+	public void setAccount()
+	{
+		
+	}
+	public void checkBalance()
+	{
+		
+	}
+	public void getAmmount()
+	{
+		
+	}
+	public void setDeposit()
+	{
+		
+	}
+	public static void main(String[] args) 
+	{
+		
+	}
+}
